0e915599194020ecb53e7f92142a5eb4419e488b
sooahkim <saligo96@gmail.com>
2019-07-23 17:50:52 +0900

    second commit

--- disneyproject/disneyproject/settings.py
+++ disneyproject/disneyproject/settings.py
@@ -20,10 +20,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2n@d)=m8&ic_&-&q(_%v!0389tz0qe^$o+8+t30ri4v=@*w%ni'
+#SECRET_KEY = '2n@d)=m8&ic_&-&q(_%v!0389tz0qe^$o+8+t30ri4v=@*w%ni'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','2n@d)=m8&ic_&-&q(_%v!0389tz0qe^$o+8+t30ri4v=@*w%ni')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
+
 
 ALLOWED_HOSTS = []
 
